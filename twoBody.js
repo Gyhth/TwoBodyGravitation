efc86c5372fc6138e8949d605de00567eb4e0f63
Gyhth <gerald.merkley@gmail.com>
2018-09-08 11:52:24 -0400

    Changed the equasions
    Altered the Equasions and handling so that it better reflects two body gravitation between the two circles. The larger circle (Circle 1) should move, but only minimally.

--- twoBody.js
+++ twoBody.js
@@ -8,7 +8,7 @@ function gravityClass(planetOne, planetTwo) {
 	this.planetTwo = planetTwo;
 };
 
-function planetClass(mass, size){ 
+function planetClass(mass, size){
 	this.mass = mass,
 	this.size = size,
 	this.x = 0
@@ -24,7 +24,7 @@ function init(parentObject, width, height) {
 		this.height = height;
 		$(parentObject).append("<canvas id=\"gravity\" width=\""+width+"\" height=\""+height+"\"></canvas>");
 		this.canvas = document.getElementById("gravity");
-		var context = this.canvas.getContext("2d");	
+		var context = this.canvas.getContext("2d");
 		context.beginPath();
 		context.fillStyle = "#FF0000";
 		planetOne.x = planetOne.size;
@@ -37,16 +37,16 @@ function init(parentObject, width, height) {
 		context.fill();
 		setInterval(this.calculate, 1);
 }
-	
+
 function calculate() {
 		var canvas = document.getElementById("gravity");
 		context = canvas.getContext("2d");
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		var distance = planetOne.x - planetTwo.x;
-		var force = 9.8*(planetOne.mass/(planetOne.mass+planetTwo.mass))/distance;
-		planetOne.x -= force; 
+		var force = 9.8*(planetTwo.mass/(planetOne.mass+planetTwo.mass))/distance;
+		planetOne.x -= force;
 		if (planetOne.x < 0) {
-			planetOne.x = planetOne.size;
+			planetOne.x = 0;
 		}
 		if (planetOne.x > canvas.width) {
 			planetOne.x = canvas.width - planetOne.x;
@@ -54,21 +54,23 @@ function calculate() {
 		if (planetOne.x == 0) {
 			planetOne.x = planetOne.size;
 		}
+		console.log(planetOne.x);
 		context.beginPath();
 		context.fillStyle = "#FF0000";
 		context.arc(planetOne.x, canvas.height/2, planetOne.size, 0, 2 * Math.PI, false);
-		context.fill();	
+		context.fill();
 		context.beginPath();
 		context.fillStyle = "#0000FF";
+		force = 9.8*(planetOne.mass/(planetOne.mass+planetTwo.mass))/distance;
 		planetTwo.x += force;
 		if (planetTwo.x < 0) {
-			planetTwo.x = planetOne.size;
+			planetTwo.x = 0;
 		}
 		if (planetTwo.x > canvas.width) {
-			planetTwo.x = canvas.width - planetOne.x;
+			planetTwo.x = canvas.width - planetTwo.x;
 		}
 		if (planetTwo.x == 0) {
-			planetOne.x = planetOne.size;
+			planetTwo.x = planetTwo.size;
 		}
 		context.arc(planetTwo.x, canvas.height/2, planetTwo.size, 0, 2 * Math.PI, false);
 		context.fill();
